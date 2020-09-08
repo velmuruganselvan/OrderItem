be4564e734b13eba57a6860dd407e5c077db3135
Velmurugan <53471249+velmurugansuresh@users.noreply.github.com>
2020-09-08 18:07:45 -0500

    Initial Commit

---
+++ src/main/java/com/oms/orderitem/OrderitemApplication.java
@@ -0,0 +1,13 @@
+package com.oms.orderitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderitemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderitemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oms/orderitem/OrderitemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.oms.orderitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class OrderitemApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
